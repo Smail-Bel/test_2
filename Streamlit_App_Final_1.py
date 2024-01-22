07c62fcf28a63808e4370a78681dcc4be2b40c7c
Smail Belouch <belouchsmail@gmail.com>
2024-01-22 15:25:07 +0100

    add_new_model

---
+++ Streamlit_App_Final_1.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3773469deef5f81117888a1f65a8654eef81a7ac7f10cc81cbb4721f9aef3d57
+size 1927
---
+++ Streamlit_App_Final_2.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e242494eee5ca10329186e6c73fcf399657725d6ca6f557d6596bfd140363819
+size 1927
