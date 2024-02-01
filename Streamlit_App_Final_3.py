9346453c5cd06c1fabd7785925fcec15a4a61305
Smail <ss.smail.natsu@gmail.com>
2024-02-01 17:54:28 +0100

    n

--- Streamlit_App_Final_3.py
+++ Streamlit_App_Final_3.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50ebffd4790b8144fd4a1a19bcc7d1f9990e8cbe925a8407f5455d265bcfaca6
-size 1705
+oid sha256:2b23d64c4ebf4fb01b55d13bfc340306c52c8b30b854204d3d01d72ce4b63133
+size 1704
---
+++ streamlit_app_1.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:698ca276f5dd57b8334590861fb80ae01c4c6dc18f67287cef33bacf8b54d76d
+size 1822
